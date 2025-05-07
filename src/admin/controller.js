7fbcdd5266b8e5f0d9d98dbd1f23a6fad1d73697
dedusis <leodedusis@gmail.com>
2025-05-08 01:01:52 +0300

    added create and getall drivers functions

--- src/admin/controller.js
+++ src/admin/controller.js
@@ -1,8 +1,27 @@
 const { getAllUsers } = require('./service');
+const {createDriver} = require('./service');
 
-const getAllUsersController = (req, res) => {
-  const drivers = getAllUsers();
-  res.send(drivers);
+const getAllUsersController = async (req, res) => {
+  try {
+    const drivers = await getAllUsers();
+    console.log("Drivers list:", drivers);
+    res.status(200).json(drivers);
+  } catch (err) {
+    console.error('Error fetching drivers:', err);
+    res.status(500).json({ error: 'Server error while fetching drivers' });
+  }
 };
 
-module.exports = { getAllUsersController };
+const createDriverController = async (req, res) => {
+  try {
+    const newDriver = await createDriver(req.body);
+    console.log("creating driver:", newDriver);
+    res.status(201).json({ Message: 'Driver created successfully', driver: newDriver });
+  } catch (err) {
+    console.error('Error creating driver:', err);
+    res.status(500).json({ error: 'Server error while creating driber' });
+  }
+};
+
+
+module.exports = { getAllUsersController, createDriverController };
--- src/admin/route.js
+++ src/admin/route.js
@@ -1,10 +1,14 @@
 const express = require('express')
-const { getAllUsersController  } = require('./controller');
 const router = express.Router();
+const { getAllUsersController  } = require('./controller');
+const { createDriverController } = require('./controller');
 
 
 //ger all drivers
-router.get("/", getAllUsersController );
+router.get('/alldrivers', getAllUsersController);
+
+//create driver
+router.post('/newdriver', createDriverController);
 
 
-module.exports = router;
+module.exports = router; 
