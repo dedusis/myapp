68c71e5cd746b91cafb6a0f4a72c68bbc2e667b9
pzogop <pzogop@gmail.com>
2025-05-14 00:28:31 +0300

    ALLAGH

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -19,7 +19,7 @@ export const verifyToken = (req, res, next) => {
 
 export const isAdmin = (req, res, next) => {
   if (req.user.role !== 'admin') {
-    return res.status(403).json({ error: 'Access denied. Admins only.' });
+    return res.status(403).json({ error: 'Access denied. Admins only nai.' });
   }
   next();
 };
